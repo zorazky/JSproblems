25ee39b3618952713c8b26a44e8f111e285d6e41
Oswaldo <oswaldo.vazquez@gmail.com>
2019-07-13 09:21:46 -0500

    camel case

---
+++ camelCase.js
@@ -0,0 +1,17 @@
+function camelcase(s) {
+
+    let counter = 1;
+
+    for (let i = 0; i < s.length; i++) {
+        if (s.charCodeAt(i) <= 90 && s.charCodeAt(i) >= 65) {
+            counter++
+        }
+    }
+
+    console.log(counter)
+    return counter
+
+}
+
+
+camelcase("saveChangesInTheEditor")//5
