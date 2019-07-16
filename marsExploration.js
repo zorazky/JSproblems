b2a2b3f6ab27c13ceaf0ab8dce52ed66dd60327f
Oswaldo <oswaldo.vazquez@gmail.com>
2019-07-15 23:17:14 -0500

    mars exploration not working yet

---
+++ marsExploration.js
@@ -0,0 +1,28 @@
+function marsExploration(s) {
+    /*
+    let counter = 0;
+    let repeated = Math.floor(s.length / 3)
+   
+    for (let j = 0; j < repeated; j++) {
+      
+      for (let i = 0; i < 3; i++) {
+        if ((i === 0 || i === 2) && s[i] !== "S") {
+          
+          counter++
+        } 
+        
+        if (i === 1 && s[i] !== "O") {
+          
+          counter++
+        } 
+      }
+    }
+    
+    
+   */
+    
+    console.log("counter is " + counter)
+    return counter
+  }
+  
+  marsExploration("SOSSPSSQSSOR"); // 3 SOS-SPS-SQS-SOR
