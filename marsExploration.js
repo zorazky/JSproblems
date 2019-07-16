e9862702d9b98310fbd72f1c373e3d120d9c1646
Oswaldo <oswaldo.vazquez@gmail.com>
2019-07-15 23:27:47 -0500

    mars exploration working

--- marsExploration.js
+++ marsExploration.js
@@ -1,25 +1,27 @@
 function marsExploration(s) {
-    /*
+  
     let counter = 0;
     let repeated = Math.floor(s.length / 3)
-   
-    for (let j = 0; j < repeated; j++) {
-      
-      for (let i = 0; i < 3; i++) {
-        if ((i === 0 || i === 2) && s[i] !== "S") {
+    
+     for (let j = 0; j < repeated; j++) {
+       
+       //for (let i = j * 3; i < (j * 3) + 3; i++) {
+        for (let i = 0; i < 3; i++) {
+         console.log(j * 3 + i)
+         
+         if ((i === 0 || i === 2) && s[j * 3 + i] !== "S") {
           
           counter++
         } 
         
-        if (i === 1 && s[i] !== "O") {
+        if (i === 1 && s[j * 3 + i] !== "O") {
           
           counter++
         } 
-      }
-    }
-    
-    
-   */
+       }
+         
+       
+     }
     
     console.log("counter is " + counter)
     return counter
