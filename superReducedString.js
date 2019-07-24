fa1f731191115baef9661cd09b5525e36cdc7d00
Oswaldo <oswaldo.vazquez@gmail.com>
2019-07-23 22:44:23 -0500

    super reduced string working with recursion :)

--- superReducedString.js
+++ superReducedString.js
@@ -1,46 +1,27 @@
-let newWord = "";
-  const total = {}
-  
-  
-  
-  let newArr = s.split('');
-  
-  for (let i = 0; i< newArr.length; i++) {
-    for (let j = i + 1; j < newArr.length; j++) {
-      
-      if(newArr[i] === newArr[j]) {
-        
-        newArr.splice(j, 1);
-        newArr.splice(i, 1);
-        
-      }
-    }
-  }
-  
-  
-  newWord = newArr.join("")
-  
-  
-  for (let i = 0; i < newWord.length; i++) {
-    if(total[newWord[i]]) {
-      total[newWord[i]] += 1
-    } else {
-      total[newWord[i]] = 1
-    }
-  }
-  
-  
-  for (var property1 in total) {
-    console.log(total[property1]);
-    if (total[property1] > 1) {
-      
-      superReducedString(newWord)
-    } else {
-      
-      return newWord.length > 0 ? newWord : "Empty String"
-      
-    }
-  }
+function superReducedString(s) {
 
-  superReducedString("aaabccddd")//abd
-superReducedString("baab")//empty
+  let myArr = s.split("");
+  let finalWord = ""
+  
+  const checkPair = (arr) => {
+    for (let i = 0; i < arr.length - 1; i++) {
+      if (arr[i] === arr[i+1]) {
+         arr.splice(i,2);  
+      
+         checkPair(arr)
+      }
+         
+    }  
+     return arr.join("")
+  }
+   
+   finalWord = checkPair(myArr);
+   
+   console.log(finalWord)
+   return finalWord ? finalWord : "Empty String"
+ }
+ 
+ 
+ superReducedString("aaabccddd")//abd
+ superReducedString("baab")//empty
+ superReducedString("aa")//empty
