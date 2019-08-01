20f4614f72235f39ed286ba38dd242be70aaf84d
Oswaldo <oswaldo.vazquez@gmail.com>
2019-08-01 14:53:41 -0500

    pretty sure I'll need recursion here

---
+++ chocolateFest.js
@@ -0,0 +1,24 @@
+function chocolateFeast(n, c, m) {
+
+  let dollars = n
+  let chocolateCost = c
+  let chocolateWrap = m
+  let initialWraps = Math.floor((dollars / chocolateCost) / chocolateWrap)
+  let chocolateCounter = n / c
+  let wrapperLeft = chocolateCounter > chocolateWrap ? chocolateCounter % chocolateWrap : 0
+  
+  
+  const chocolateChange = (cC, iW ) => {
+    
+    if (initialWraps + wrapperLeft )
+    return Math.floor(cC + iW)
+  }
+  
+  console.log(wrapperLeft)
+  //console.log(chocolateChange(chocolateCounter,initialWraps))
+}
+
+chocolateFeast(10, 2, 5);//6
+chocolateFeast(12, 4, 4);//3
+chocolateFeast(6, 2, 2);//5
+chocolateFeast(7, 3, 2);//3
