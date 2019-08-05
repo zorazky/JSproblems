3d496695de15df9285d128de7f53891961939a8b
Oswaldo <oswaldo.vazquez@gmail.com>
2019-08-04 21:36:19 -0500

    almost there

--- jumpingOnClouds.js
+++ jumpingOnClouds.js
@@ -4,26 +4,29 @@ function jumpingOnClouds(c) {
     let counter = 0
   
     while (true) {
-      console.log(i)
-      console.log("counter is " + counter)
+      
+      //console.log(i)
+      //console.log("counter is " + counter)
       
       
-      if (c[i + 2] !== 1 && c[i + 2] < c.length ) {
+      if (c[i + 2] !== 1 && c[i + 2] <= c.length ) {
         i += 2
         counter++
-      } else if (c[i + 1] !== 1 && c[i + 2] < c.length ) {
+      } else if (c[i + 1] !== 1 && c[i + 2] <= c.length ) {
         i += 1
         counter++
-      }
-      if (i === c.length + 1) {
+      } else {
         break;
       }
+      
     }
     
-    //console.log(counter)
+    console.log(counter)
+    return counter
     
     
   }
   
   jumpingOnClouds([0,0,1,0,0,1,0]);//4
   jumpingOnClouds([0,0,0,0,1,0]);//3
+  jumpingOnClouds([0,0,0,1,0,0]);//3
