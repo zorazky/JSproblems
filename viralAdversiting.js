7e916a7fa600802f53343459fb9e8fda233cc9bc
Oswaldo <oswaldo.vazquez@gmail.com>
2019-08-11 22:20:07 -0500

    viral adversiting basic idea just not working yet

---
+++ viralAdversiting.js
@@ -0,0 +1,33 @@
+function viralAdvertising(n) {
+
+    //console.log((Math.floor(5/2)*3))
+    
+    let counter = 0
+    let liked = 2
+    let tempShared = 5
+    
+    while (counter < n) {
+      
+     
+      
+      counter++
+    }
+    
+    console.log(liked);
+    
+    
+  }
+  
+  viralAdvertising(3)//9
+  //viralAdvertising(4)//15
+  //viralAdvertising(5)//24
+  
+  
+  /*
+  Day Shared Liked Cumulative
+  1      5     2       2
+  2      6     3       5
+  3      9     4       9
+  4     12     6      15
+  5     18     9      24
+  */
