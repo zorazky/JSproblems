232a4433e66514a94860c532cc6a10c089d1e252
Oswaldo <oswaldo.vazquez@gmail.com>
2019-08-22 23:44:21 -0500

    kinda working just need to read the inputs correctly

--- climbingLeaderboard.js
+++ climbingLeaderboard.js
@@ -1,16 +1,28 @@
 function climbingLeaderboard(scores, alice) {
 
     let place = 1
+  
+    const scoresUnique = scores.filter(function(item, index){
+      return scores.indexOf(item) >= index;
+    });
+    
+    //console.log(scoresUnique)
+    
     
     alice.map(score => {
-      for (let i = 0; i < scores.length; i++) {
-        if (score < scores[i]) {
+      for (let i = 0; i < scoresUnique.length; i++) {
+        //console.log(`score is :${score} and place:${place} from ${scoresUnique[i]}`)
+        if (score < scoresUnique[i]) {
           place++
-        } else {
-          console.log(place)
         }
+        //console.log(place)
       }
+      
+      console.log(place)
+      place = 1
     })
+    
+     
      
   }
   
